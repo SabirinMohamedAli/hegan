292d381a2d293bd5808b90ce2e933146e231635e
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2025-02-11 17:28:56 +0300

    i begin the code

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+const express = require('express');
+const mongoose= require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//db connection
